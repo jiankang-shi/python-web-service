a4923c3b417fefe8b714ab21328c779d0f276610
Administrator <1049547118@qq.com>
2021-10-19 11:18:15 +0800

    version-done

--- main/kafka-Monitor-web-service/addPartitionWebService.py
+++ main/kafka-Monitor-web-service/addPartitionWebService.py
@@ -24,11 +24,9 @@ logging.basicConfig(level=logging.INFO,
 @app.route('/addpartitions/<topic>')
 def index(topic):
     try:
-        value = request.args.get("l")
-        logging.info("xxxxxv%s:%s" % (topic, value))
-
-        logging.info("1********start--select:%s********" % (topic))
-        # pg_select(topic,value)
+        flow = request.args.get("l")
+        logging.info("1********start--select:%s & flow:%s********" % (topic, flow))
+        pg_select(topic, flow)
         code = 200
         # return "<h1 style='color:red'> %s %s </h1> " %(topic,code)
         return "topic_name:%s,Right_code:%s" % (topic, code)
@@ -37,24 +35,24 @@ def index(topic):
         return "topic_name:%s,Error_code:%s" % (topic, code)
 
 
-
 def sendqt(infor):
     startTime = (time.strftime("%Y-%m-%d-%H:%M:%S", time.localtime()))
     cmd = 'sh  /opt/work/addPQT.sh %s ' % infor
     (status, output) = subprocess.getstatusoutput(cmd)
 
 
-def add_partition(topic, Pum):
+def add_partition(topic_name, now_partitions, add_partitions):
     # client = KafkaAdminClient(bootstrap_servers="l-logcollectkafka1.ops.cna:9092",logger=info)
     client = KafkaAdminClient(bootstrap_servers="l-logcollectkafka1.ops.cna:9092")
-    client.create_partitions({topic: NewPartitions(Pum)})
-    addinfolog = 'topic_name:%s add_partitions:%s success' % (topic, Pum)
-    sendilog = 'info:[topic_name:%s##add_partitions:%s##]' % (topic, Pum)
+    client.create_partitions({topic_name: NewPartitions(add_partitions)})
+    addinfolog = '6********topic_name:%s add_partitions:%s success' % (topic_name, add_partitions)
+    sendilog = 'info:[topic_name:%s##add_partitions_after_num:%s##]' % (topic_name, add_partitions)
     logging.info(addinfolog)
+    pg_insert(topic_name, now_partitions, add_partitions)
     sendqt(sendilog)
 
 
-def pg_select(topic,value):
+def pg_select(topic, flow):
     conn = psycopg2.connect(dbname="logsysmeta", user="platform_bigdata_prd",
                             password="2d7e3ec9-6256-400a-bba8-09a923b35203", host="l-bigdatadbvip4.pf.cn6",
                             port="5432")
@@ -68,7 +66,7 @@ def pg_select(topic,value):
     # print (temp[0][0])
     if str((temp[0][0])) == 'None' or int((temp[0][0])) < 50:
         logging.info("2********select--db:%s:is None/<50 ********" % (topic))
-        get_partition(topic,value)
+        get_partition(topic, flow)
     else:
         now = (time.strftime("%Y-%m-%d-%H:%M:%S", time.localtime()))
         addwarnlog = '********END**********topic:%s Today_is_add_partition_exceed_50********' % topic
@@ -85,48 +83,65 @@ def pg_insert(topic_name, now_partitions, add_partitions):
     cursor = conn.cursor()
     cursor.execute("INSERT INTO kafka_partitions (cluster_name,topic_name,now_partitions,update_time,add_partitions) \
                    VALUES (%s,%s,%s,now(),%s)", (cluster_name, topic_name, now_partitions, add_partitions))
-    insertdblog = "********END   INSERT DB  cluster_name:%s topic_name:%s now_partitions:%s add_partitions:%s success-END" % (
-    cluster_name, topic_name, now_partitions, add_partitions)
+    insertdblog = "7********END   INSERT DB  cluster_name:%s topic_name:%s now_partitions:%s add_partitions:%s success-END" % (
+        cluster_name, topic_name, now_partitions, add_partitions)
     logging.info(insertdblog)
     conn.commit()
     cursor.close()
     conn.close()
 
 
-def get_partition(topic,value):
+def currency(topic, singlePflow, flowss, currentP):
+    if singlePflow > 10485760:
+        runlog = "3********current total-flow:%s and current partition:%s and Average flow:%s" % (
+            flowss, currentP, singlePflow)
+        logging.info(runlog)
+        estimateP = int(flowss / 10485760)
+        addP = estimateP - currentP
+        addmessage = "4********add message addP=estimateP - currentP:%s= %s - %s" % (addP, estimateP, currentP)
+        logging.info(addmessage)
+        if addP == 0:
+            saddplog = "5********START topic_name:%s  add partition:%s" % (topic, 1)
+            logging.info(saddplog)
+            add_partition(topic, currentP, currentP + 1)
+            eaddplog = "8********END   topic_name:%s  add partition:%s done" % (topic, 1)
+            logging.info(eaddplog)
+        else:
+            saddplog = "5********START topic_name:%s  add partition:%s" % (topic, addP)
+            logging.info(saddplog)
+            add_partition(topic, currentP, currentP + addP)
+            eaddplog = "8********END   topic_name:%s  add partition:%s done" % (topic, addP)
+            logging.info(eaddplog)
+
+    else:
+        exitlog = "topic:%s Average flow:%s 不大于10M" % (topic, singlePflow)
+        logging.info(exitlog)
+
+
+def get_partition(topic, flow):
     zk = KazooClient(hosts='l-logcollectkafka1.ops.cna')
     zk.start()
     zkdir = '/brokers/topics/%s/partitions' % topic
     if zk.exists(zkdir):
         try:
-            # print ("存在")
             node = zk.get_children('/brokers/topics/%s/partitions' % topic)
-            # print (node)
             data = map(eval, node)
             currentP = (max(data) + 1)
-            # needaddp = currentP // 2
-            needaddp = (currentP  * value)/10
-            # print (currentP,needaddp)
-            if needaddp == 0:
-                saddplogz = "zero********START topic_name:%s  add partition:%s" % (topic, needaddp)
-                logging.info(saddplogz)
-                add_partition(topic, currentP + 1)
-                eaddplogz = "zero********END  topic_name:%s  add partition:%s" % (topic, needaddp)
-                logging.info(eaddplogz)
+            inforpartition = "********topic:%s current parition:%s " % (topic, currentP)
+            logging.info(inforpartition)
+            if "M" in flow:
+                flows = flow.replace("M", "")
+                flowss = float(flows) * 1024 * 1024  # 换算总流量
+                singlePflow = int(flowss / currentP)  # 平均分区流量
+                currency(topic, singlePflow, flowss, currentP)
 
-                sdblog = "zero********start  INSERT DB information"
-                logging.info(sdblog)
-                pg_insert(topic, currentP, needaddp)
-            else:
-                saddplog = "3********START topic_name:%s  add partition:%s" % (topic, needaddp)
-                logging.info(saddplog)
-                add_partition(topic, currentP + needaddp)
-                eaddplog = "4********END   topic_name:%s  add partition:%s" % (topic, needaddp)
-                logging.info(eaddplog)
 
-                sdblog = "5********start  INSERT DB information"
-                logging.info(sdblog)
-                pg_insert(topic, currentP, needaddp)
+            elif "G" in flow:
+                flows = flow.replace("G", "")
+                flowss = float(flows) * 1024 * 1024 * 1024  # 换算总流量
+                singlePflow = int(flowss / currentP)  # 平均分区流量
+                currency(topic, singlePflow, flowss, currentP)
+
 
         except Exception as err:
             print(err.message)
